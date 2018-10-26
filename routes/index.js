a1ef808daf94b336e1950a338fc13ccaef0c204b
INDIA GUY <indiaguy@SCENEITs-iMac.local>
2018-10-25 23:56:57 -0400

    edied routes

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,22 @@ router.get('/users', function(req, res, next) {
   res.render('users/index', { title: 'Users' });
 });
 
+// const applicationController = require('../controllers/application.js')
+// const userController = require('../controllers/users.js')
+// const itemsController = require('../controllers/items.js')
 
+// //homepage
+//  router.get('/', applicationController.index)
+
+//  router.get('/users/new', userController.new)
+//  router.get('/users/:id', userController.show)
+//  router.post('/users', userController.create) 
+
+// router.get ('/items/new', itemsController.new)
+// router.get ('/items/:id', itemsController.show)
+// router.patch('/items/:id', itemsController.update)
+// router.put('/items/:id', itemsController.update)
+// router.post ('/items/:id', itemsController.update)
 
 
 
