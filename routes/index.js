741ae707a739ddce898edeeb3e21d9e660b163d5
Rguy <rguy1290@gmail.com>
2018-10-26 19:58:48 -0400

    deleted commented out code

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('app/index', { title: 'PicList' });
-// });
-
-// router.get('/users', function(req, res, next) {
-//   res.render('users/index', { title: 'Users' });
-// });
-
 const applicationController = require('../controllers/application.js')
 const usersController = require('../controllers/users.js')
 const eventsController = require('../controllers/events.js')
