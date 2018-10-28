7052ffb7592e911fd32dd076c763eb27b1b2644c
Rguy <rguy1290@gmail.com>
2018-10-27 20:26:02 -0400

    fixed post and delete for events

--- controllers/events.js
+++ controllers/events.js
@@ -20,7 +20,33 @@ const eventsController = {
         .then(event =>{
             res.render('events/show', {event: event})
         })
-    }
+    },
+
+    create: (req, res) => {
+        Event.create(req.body).then((newEvent) => {
+          res.redirect(`/${newEvent._id}`)
+        })
+      },
+    
+
+      edit: (req, res) => {
+        Event.findById(req.params.id).then(event => {
+          res.render('events/edit', { event: event })
+        })
+      },
+
+      update: (req, res) => {
+        Event.findByIdAndUpdate(req.params.id, req.body).then((updatedEvent) => {
+          res.redirect(`/${updatedEvent._id}`)
+        })
+      },
+
+      delete: (req, res) => {
+        Event.findByIdAndRemove(req.params.id).then(() => {
+          res.redirect('/')
+        })
+      }
+    
 }
 
 module.exports = eventsController
