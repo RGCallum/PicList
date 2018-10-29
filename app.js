68d0dc446fd7e9b6339862459dbe5a1808937af5
Rguy <rguy1290@gmail.com>
2018-10-29 18:25:03 -0400

    got editing event page to work by fixing input type and name

--- app.js
+++ app.js
@@ -12,13 +12,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 app.use(methodOverride('_method'))
 
-// app.use(methodOverride(function(req, res){
-//   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-//     var method = req.body._method
-//     delete req.body._method
-//     return method
-//   }
-// }))
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
