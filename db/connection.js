77021128c39a40b51b88e7e1df797ddcde64057a
Rguy <rguy1290@gmail.com>
2018-10-30 16:58:45 -0400

    Updated DB connection code to use MongoLab when in Production

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
-
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-console.log('Connected to MongoDB')
-})
+
+// Connect to database
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/PicList');
+  }
+  mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+  })
 
 
 
